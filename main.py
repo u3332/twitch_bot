a71d1871266665df590d2a47d9221dbe40badf74
ruslanyeremichuk <ruslanyeremichuk@gmail.com>
2024-05-19 23:25:22 +0300

    update phrases

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from contextlib import asynccontextmanager
 
 
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI, __version__, HTTPException
+from fastapi import FastAPI, __version__
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from sqlalchemy.future import select
@@ -80,54 +80,41 @@ async def get_prediction():
     return {'prediction': prediction}
 
 
-@app.post("/update_points/{username}", response_model=str)
-def update_points(username: str, db: DBSessionDep):
+@app.post("/update_length/{username}", response_model=str)
+def update_length(username: str, db: DBSessionDep):
     try:
         user = db.execute(select(PenisData).filter(PenisData.username == username)).scalar()
+        penis_prefix = random.choice(['піструн', 'шланг', 'член', 'удав', 'прутень', 'вялий', 'стержень', 'стрижень', 'ствол', 'блохастий', 'волохатий', 'лисий'])
+
 
         if not user:
-            new_length = random.uniform(-10, 10)
+            new_length = random.randint(-10, 10)
             user = PenisData(username=username, length=new_length)
             db.add(user)
             db.commit()
             db.refresh(user)
-            return f"{username}, у тебе з'явився член, і його довжина - {new_length:.2f} см."
+            return f"{username}, у тебе з'явився {penis_prefix}, і його довжина - {new_length} см."
 
         now = datetime.utcnow()
-        if user.last_updated and (now - user.last_updated) < timedelta(hours=24):
-            remaining_time = timedelta(hours=24) - (now - user.last_updated)
-            hours, remainder = divmod(remaining_time.seconds, 3600)
-            minutes, seconds = divmod(remainder, 60)
-            db.rollback()  # Explained below
-            return f"Ти маєш шанс збільшити свій член тільки 1 раз в 24 години, почекай ще {hours} годин, {minutes} хвилин, {seconds} секунд."
+        if user.last_updated and (now - user.last_updated) < timedelta(hours=10):
+            return f"Лінійка зламалася, перевіриш завтра! Наразі в тебе: {user.length} см"
 
-        change = round(random.uniform(-10, 10), 2)
+        change = random.randint(-10, 10)
         user.length += change
         user.last_updated = now
         db.commit()
         db.refresh(user)
         if change >= 0:
-            return f"{username}, я тебе вітаю! Твій член виріс на {abs(change):.2f} см. Поточна довжина: {user.length:.2f} см."
+            return f"{username}, я тебе вітаю! Твій {penis_prefix} виріс на {abs(change)} см. Поточна довжина: {user.length} см."
         elif change == 0:
-            return f"{username}, вважай, що тобі почастило. Твій член не змінився в розмірі. Поточна довжина: {user.length:.2f} см."
+            return f"{username}, вважай, що тобі почастило. Твій {penis_prefix} не змінився в розмірі. Поточна довжина: {user.length} см."
         else:
-            return f"{username}, їбать ти лох! Твій член зменшився на {abs(change):.2f} см. Поточна довжина: {user.length:.2f} см."
+            return f"{username}, їбать ти лох! Твій {penis_prefix} зменшився на {abs(change)} см. Поточна довжина: {user.length} см."
     except SQLAlchemyError as e:
         db.rollback()
         logging.error(f"Database error: {str(e)}", exc_info=True)
         return "Помилка бази даних, вибачай!"
     except Exception as e:
         db.rollback()
-        logging.error(f"Unexpected error: {str(e)}", exc_info=True)
-        return "Неочікувана помилка, вибачай!"
-
-@app.get("/points/{username}", response_model=str)
-def get_points(username: str, db: DBSessionDep):
-    try:
-        user = db.execute(select(PenisData).filter(PenisData.username == username)).scalar()
-        if not user:
-            raise HTTPException(status_code=404, detail="Користувача не знайдено")
-        return f"{username}, довжина твого члена: {user.length:.2f} см."
-    except Exception as e:
         logging.error(f"Unexpected error: {str(e)}", exc_info=True)
         return "Неочікувана помилка, вибачай!"
--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from sqlalchemy import Column, String, Float, Integer, DateTime
+from sqlalchemy import Column, String, Integer, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -9,5 +9,5 @@ class PenisData(Base):
     __tablename__ = "penis_data"
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(25))
-    length = Column(Float, default=0)
+    length = Column(Integer, default=0)
     last_updated = Column(DateTime, default=datetime.utcnow)
